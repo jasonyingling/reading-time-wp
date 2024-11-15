2e53d5e9abae205060531dd25e1b4bd45fc09223
Jason Yingling <jyingling@hlkagency.com>
2024-11-15 16:49:56 -0600

    tagging for 2.0.17 release

--- rt-reading-time.php
+++ rt-reading-time.php
@@ -7,7 +7,7 @@
  * Plugin Name: Reading Time WP
  * Plugin URI: https://jasonyingling.me/reading-time-wp/
  * Description: Add an estimated reading time to your posts.
- * Version: 2.0.16
+ * Version: 2.0.17
  * Author: Jason Yingling
  * Author URI: https://jasonyingling.me
  * License: GPL2
